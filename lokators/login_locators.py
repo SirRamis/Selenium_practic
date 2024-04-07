b7eb9c6256e030f0c02952245a01370322cbc548
Ramis <sir.ramis@mail.ru>
2024-04-07 11:29:03 +0300

    add class LoginLocators

---
+++ lokators/login_locators.py
@@ -0,0 +1,6 @@
+
+
+class LoginLocators:
+    USERNAME_FIELD = ('xpath', '//*[@id="user-name"]')
+    PASSWORD_FIELD = ('xpath', '//*[@id="password"]')
+    LOGIN_BUTTON = ('xpath', '//*[@id="login-button"]')
